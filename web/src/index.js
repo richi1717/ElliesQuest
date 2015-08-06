a6fe3f0b0e480345ec3e8c45b05138480efa8540
Richi Jeffery <kikki_sawn@yahoo.com>
2015-08-06 02:24:13 -0700

    add some basic building blocks for battle system

--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,7 @@
 
 var $ = require('jquery')
 var Backbone = require('backbone')
-var _ = require('underscore')
+var _ = require('lodash')
 
   var Direction = {
         top: 12,
@@ -25,11 +25,11 @@ var characterStats = {
 
 
 var cells = [
-  {x: 0, y: 0, terrain: "sand", traversable:true},
+  {x: 0, y: 0, terrain: "forest", traversable:true},
   {x: 1, y: 0, terrain: "sand", traversable:true},
-  {x: 2, y: 0, terrain: "grass", traversable:true},
-  {x: 3, y: 0, terrain: "grass", traversable:true},
-  {x: 4, y: 0, terrain: "grass", traversable:true},
+  {x: 2, y: 0, terrain: "forest", traversable:true},
+  {x: 3, y: 0, terrain: "forest", traversable:true},
+  {x: 4, y: 0, terrain: "forest", traversable:true},
   {x: 5, y: 0, terrain: "water", traversable:false},
   {x: 6, y: 0, terrain: "grass", traversable:true},
   {x: 7, y: 0, terrain: "sand", traversable:true},
@@ -45,8 +45,8 @@ var cells = [
 
   {x: 0, y: 1, terrain: "sand", traversable:true},
   {x: 1, y: 1, terrain: "grass", traversable:true},
-  {x: 2, y: 1, terrain: "grass", traversable:true},
-  {x: 3, y: 1, terrain: "grass", traversable:true},
+  {x: 2, y: 1, terrain: "forest", traversable:true},
+  {x: 3, y: 1, terrain: "forest", traversable:true},
   {x: 4, y: 1, terrain: "water", traversable:false},
   {x: 5, y: 1, terrain: "water", traversable:false},
   {x: 6, y: 1, terrain: "water", traversable:false},
@@ -209,6 +209,11 @@ $(function () {
   
   $('body').on('keydown', function (e) {
     $('p').removeClass('top left right bottom')
+    // check each step for a battle event
+    if (_.random(0, 10) === 1) {
+      console.log('battle')
+      enemyNameByArea()
+    }
     var direction = 'top'
     if (e.keyCode === 38) {
       direction = 'top'
@@ -266,6 +271,55 @@ $(function () {
     }
     return { x: x, y: y }
   }
+
+  function battle() {
+    var random = _.random(1, 20)
+    var inRange = _.inRange(random, 10, 20)
+    if (inRange) {
+      console.log('hit')
+    } else {
+      console.log('miss')
+    }
+
+
+  }
+  // will determine what type of enemy they will be
+  function enemyNameByArea() {
+    if (_.random(1, 6) === 1) {
+      console.log('you fight gorgon')
+      battle()
+    } else if (_.random(1, 6) === 2) {
+      console.log('you fight templar')
+      battle()
+    } else if (_.random(1, 6) === 3) {
+      console.log('you fight wolf')
+      battle()
+    } else if (_.random(1, 6) === 4) {
+      console.log('you fight dog')
+      battle()
+    } else if (_.random(1, 6) === 5) {
+      console.log('you fight snake')
+      battle()
+    } else {
+      console.log('you fight yahoos')
+      battle()
+    }
+  }
+
+  // // checks to see if enemy is defeated or if character is defeated
+  // if (enemy.hp === 0) {
+  //   if (enemyTotal === 0) {
+  //     console.log('you win')
+  //   } else {
+  //     console.log('continue the fight')
+  //   }
+  // } else if (characterStats.hp === 0) {
+  //   if (characterTotal === 0) {
+  //     console.log('Game Over')
+  //   } else {
+  //     console.log('You are dead but your buddy lives on')
+  //   }
+  // }
 
   moveTo(characterStats.position)
 

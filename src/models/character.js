bc73ceb3d1ef752f0d3ae927ab522848331533be
Richi Jeffery <kikki_sawn@yahoo.com>
2015-08-20 00:31:04 -0700

    change the bgc of home page viewport and body change the colors of the buttons change the colors of the nav links change the colors of the new game screen to make it more uniform add option for base character stats when starting a new game add some fake users to database

--- src/models/character.js
+++ src/models/character.js
@@ -23,33 +23,21 @@ var App = require('../app');
 
 App.Models.Character = Backbone.Model.extend({
   url: function () { 
-    var base = App.Settings.apiRoot + '/characters';
+    var base = App.Settings.apiRoot + '/characters?userId=' + 1;
     if (this.isNew()) return base;
     return base + '/' + this.id
   },
-  // updateStats: function (stats) {
-  //   this.set('str', this.get('str') + stats.str)
-  //   this.set('def', this.get('def') + stats.def)
-  //   this.set('currentHp', this.get('maxHp'))
-  //   this.set('currentMp', this.get('maxMp'))
-  //   this.set('agility', this.get('agility') + stats.agility)
-  //   this.set('evade', this.get('evade') + stats.evade)
-  //   this.set('magic', this.get('magic') + stats.magic)
-  //   this.set('accuracy', this.get('accuracy') + stats.accuracy)
-  // },
+
+  addPosition: function (position) {
+    this.set('currentPosition', position)
+  },
 
   addExp: function (expGained, stats) {
     var currentLvl = expUtils.calcLevel(this.get('exp'))
     this.set('exp', this.get('exp') + expGained)
     var newLvl = expUtils.calcLevel(this.get('exp'))
 
-    
-
     if (newLvl > currentLvl) {
-      var _this = this
-      // $('div.battle > div').append('<div class="level-up">' 
-      //   + this.get('name') + ' got ' + expGained + 'Exp</div>')
-
       var newLevelStats = {
         str: _.random(5, 10),
         def: _.random(5, 10),
@@ -72,14 +60,6 @@ App.Models.Character = Backbone.Model.extend({
   },
 
   addLevel: function (stats) {
-    // var str1 = this.get('str')
-    // var def1 = this.get('def')
-    // var maxHp1 = this.get('maxHp')
-    // var maxMp1 = this.get('maxMp')
-    // var accuracy1 = this.get('accuracy')
-    // var magic1 = this.get('magic')
-    // var evade1 = this.get('evade')
-    // var agility1 = this.get('agility')
     this.set('str', this.get('str') + stats.str)
     this.set('def', this.get('def') + stats.def)
     this.set('currentHp', this.get('maxHp'))
@@ -88,32 +68,7 @@ App.Models.Character = Backbone.Model.extend({
     this.set('evade', this.get('evade') + stats.evade)
     this.set('magic', this.get('magic') + stats.magic)
     this.set('accuracy', this.get('accuracy') + stats.accuracy)
-    // console.log('You gained a level')
-    // newHero1 = this.attributes
-    // newStr1 = newHero1.str - str1
-    // newDef1 = newHero1.def - def1
-    // newMaxHp1 = newHero1.maxHp - maxHp1
-    // newMaxMp1 = newHero1.maxMp - maxMp1
-    // newAccuracy1 = newHero1.accuracy - accuracy1
-    // newMagic1 = newHero1.magic - magic1
-    // newEvade1 = newHero1.evade - evade1
-    // newAgility1 = newHero1.agility - agility1
-
-
-    // $('div.level-up > ').append(' and gained a Level')
-    // $('main').one('click', function () {
-    //   $('div.level-up').text('and gained a level!')
-    //   $('main').one('click', function () {
-    //     $('div.level-up').text('+' + newStr1 + 'Str ' + newDef1 + 'Def ' + newMaxHp1 + 'MaxHp ' + newMaxMp1 + 'MaxMp')
-    //     $('main').one('click', function () {
-    //       $('div.level-up').text('+' + newAccuracy1 + 'Accuracy ' + newMagic1 + 'Magic ' + newEvade1 + 'Evade ' + newAgility1 + 'Agility')
-    //     })
-    //   })
-    // })
-
-
   }
-
 });
 
 module.exports = App.Models.Character;
--- src/views/world.js
+++ src/views/world.js
@@ -8,6 +8,8 @@ var _ = require('lodash')
 var App = require('../app');
 var cellCollection = require('../collections/cell.js');
 var cellTerrain = require('../models/cell.js');
+var characterCollection = require('../collections/character.js');
+var Character = require('../models/character.js');
 
 var cellsTerrain = [];
 
@@ -28,214 +30,213 @@ var GameWorld = Backbone.View.extend({
 
   render: function () {
     var _this = this;
-    cellCollection.fetch().done(function (cell) {
-      
-      function cellDb(cells) {
-        cellCollection.forEach(function (cell, index) {
-          // console.log(cell.attributes)
 
-          cellsTerrain.push( cell.attributes );
-          // console.log(cellsTerrain[index])
-        })
-      }
-      cellDb()
+    characterCollection.fetch().done(function (character) {
+      cellCollection.fetch().done(function (cell) {
+        console.log()  
+        // characterCollection.forEach(function (character) {
+        //   console.log(character.attributes.currentPosition)
+        // })
+        function cellDb(cells) {
+          cellCollection.forEach(function (cell, index) {
+            // console.log(cell.attributes)
 
-      _this.$el.html(tmpl.worldTmpl(cellsTerrain))
-      var character = $('span')
-      var characterStats = {
-        name: 'Link',
-        str: 1,
-        def: 1,
-        direction: 'right',
-        position: {
-          x: 5,
-          y: 10
+            cellsTerrain.push( cell.attributes );
+            // console.log(cellsTerrain[index])
+          })
         }
-      }
+        cellDb()
 
-      $('body').on('keydown', function (e) {
-        // $('#overworld').removeClass('top left right bottom red-boy-up1 red-boy-down1 red-boy-left1 red-boy-right1 red-boy-up2 red-boy-down2 red-boy-left2 red-boy-right2')
-        // check each step for a battle event
+        _this.$el.html(tmpl.worldTmpl(cellsTerrain))
+        var character = $('span')
+        var characterStats = characterCollection.models[0].attributes
 
-        var direction = 'top'
-        var classDirection = 0
-        // var ffSprite = 'ff-sprite'
-        if (e.which === 38) {
-          direction = 'top'
-          $('span.ff-sprite').removeClass('red-boy-left1 red-boy-right1 red-boy-down1 red-boy-down2 red-boy-left2 red-boy-right2')
-          $('span.ff-sprite').addClass('red-boy-up1')
-          $('span.ff-sprite').toggleClass('red-boy-up2')
-        } else if (e.which === 37) {
-          direction = 'left'
-          $('span.ff-sprite').removeClass('red-boy-up1 red-boy-right1 red-boy-down1 red-boy-up2 red-boy-down2 red-boy-right2')
-          $('span.ff-sprite').addClass('red-boy-left1')
-          $('span.ff-sprite').toggleClass('red-boy-left2')
-        } else if (e.which === 39) {
-          direction = 'right'
-          $('span.ff-sprite').removeClass('red-boy-left1 red-boy-up1 red-boy-down1 red-boy-down2 red-boy-left2 red-boy-up2')
-          $('span.ff-sprite').addClass('red-boy-right1')
-          $('span.ff-sprite').toggleClass('red-boy-right2')
-        } else if (e.which === 40) {
-          direction = 'bottom'
-          $('span.ff-sprite').removeClass('red-boy-left1 red-boy-right1 red-boy-up1 red-boy-up2 red-boy-left2 red-boy-right2')
-          $('span.ff-sprite').addClass('red-boy-down1')
-          $('span.ff-sprite').toggleClass('red-boy-down2')
-        }
-          
-          var classes = [direction].join(' ')
+        $('body').on('keydown', function (e) {
+          // $('#overworld').removeClass('top left right bottom red-boy-up1 red-boy-down1 red-boy-left1 red-boy-right1 red-boy-up2 red-boy-down2 red-boy-left2 red-boy-right2')
+          // check each step for a battle event
 
-          $('#overworld').addClass(classes)
-          moveTo(findDirection(direction))
-          // }
-          if (_.random(0, 20) === 1) {
-          console.log(characterStats.position)
-          var terrain = findTerrain(findCell(characterStats.position.x, characterStats.position.y))
-
-          $('body').off()
-          clearInterval(checkSunny)
-          clearInterval(rainedOn)
-          clearInterval(raining)
-          clearInterval(lightning)
-          clearTimeout(lightningStrike)
-          clearInterval(rainOrNot)
-          clearInterval(sunny)
-          App.router.navigate('battle/' + terrain, { trigger: true })
-          // enemyNameByArea()
-        }
-
-      })
-
-      function findCell(x, y) {
-        // console.log(x, y)
-        return $('.row:nth-child(' + y + ') > div:nth-child(' + x + ')')
-
-      }
-
-      function buildTerrain(cells) {
-        return cells.forEach(function (cell) {
-          findCell(cell.x + 1, cell.y + 1).addClass(cell.terrain)
-        })
-      }
-
-      function moveTo(cell) {
-        var coords = findCell(cell.x, cell.y).offset()
-        character.css({
-          top: coords.top,
-          left: coords.left
-        })
-        characterStats.position = { x: cell.x, y: cell.y }
-        console.log(characterStats.position)
-        return characterStats.position
-      }
-
-      function findTerrain(cell) {
-        return cell.attr('class')
-      }
-
-      function findDirection(direction) {
-
-        var x = characterStats.position.x
-        var y = characterStats.position.y
-
-        if (direction === 'top') {
-          y--
-        } else if (direction === 'bottom') {
-          y++
-        } else if (direction === 'right') {
-          x++
-        } else if (direction === 'left') {
-          x--
-        }
-        return { x: x, y: y }
-      }
-
-      moveTo(characterStats.position)
-
-      buildTerrain(cellsTerrain)
-      // for the rain if I decide to use it
-      var raining = {}
-      var lightning = 0
-      var raining = 0
-      var lightningStrike = 0
-      function rain () {    
-        $('.sunny').addClass('rain')
-       
-        raining = setInterval(function () {
-          $('.sunny').toggleClass('rain2')
-        }, 200)
-
-        lightning = setInterval(function () {
-          var randomLightning = _.random(1, 20)
-          $('.sunny').removeClass('lightning')
-          if (randomLightning === 1) {
-            lightningStrike = setTimeout(function () {
-              $('.sunny').addClass('lightning')
-              console.log('lightning')
-               
-            }, 200)
+          var direction = 'top'
+          var classDirection = 0
+          // var ffSprite = 'ff-sprite'
+          if (e.which === 38) {
+            direction = 'top'
+            $('span.ff-sprite').removeClass('red-boy-left1 red-boy-right1 red-boy-down1 red-boy-down2 red-boy-left2 red-boy-right2')
+            $('span.ff-sprite').addClass('red-boy-up1')
+            $('span.ff-sprite').toggleClass('red-boy-up2')
+          } else if (e.which === 37) {
+            direction = 'left'
+            $('span.ff-sprite').removeClass('red-boy-up1 red-boy-right1 red-boy-down1 red-boy-up2 red-boy-down2 red-boy-right2')
+            $('span.ff-sprite').addClass('red-boy-left1')
+            $('span.ff-sprite').toggleClass('red-boy-left2')
+          } else if (e.which === 39) {
+            direction = 'right'
+            $('span.ff-sprite').removeClass('red-boy-left1 red-boy-up1 red-boy-down1 red-boy-down2 red-boy-left2 red-boy-up2')
+            $('span.ff-sprite').addClass('red-boy-right1')
+            $('span.ff-sprite').toggleClass('red-boy-right2')
+          } else if (e.which === 40) {
+            direction = 'bottom'
+            $('span.ff-sprite').removeClass('red-boy-left1 red-boy-right1 red-boy-up1 red-boy-up2 red-boy-left2 red-boy-right2')
+            $('span.ff-sprite').addClass('red-boy-down1')
+            $('span.ff-sprite').toggleClass('red-boy-down2')
           }
-           
-        }, 300)
-        console.log('raining')
-      }
+            
+            var classes = [direction].join(' ')
 
-      var randomRain = _.random(0)
-      var rainOrNot = 0
-      rainOrNot = setInterval(function () {
-        randomRain = _.random(0, 5)
-        if (randomRain === 1) {
-          if($('.sunny').hasClass('rain')) return false
-          else {
-            object.trigger("run");
-          }  
+            $('#overworld').addClass(classes)
+            moveTo(findDirection(direction))
+            // }
+            if (_.random(0, 20) === 1) {
+            console.log(characterStats.currentPosition)
+            var terrain = findTerrain(findCell(characterStats.currentPosition.x, characterStats.currentPosition.y))
+
+            $('body').off()
+            clearInterval(checkSunny)
+            clearInterval(rainedOn)
+            clearInterval(raining)
+            clearInterval(lightning)
+            clearTimeout(lightningStrike)
+            clearInterval(rainOrNot)
+            clearInterval(sunny)
+            characterModels = characterCollection.models;
+            characterModels[0].addPosition(characterStats.currentPosition)
+            App.router.navigate('battle/' + terrain, { trigger: true })
+            // enemyNameByArea()
+          }
+
+        })
+
+        function findCell(x, y) {
+          // console.log(x, y)
+          return $('.row:nth-child(' + y + ') > div:nth-child(' + x + ')')
+
         }
-      }, 2000)
 
-      var randomSunny = 0
-      var sunny = 0
-      var rainedOn = 0
-      var checkSunny = 0
+        function buildTerrain(cells) {
+          return cells.forEach(function (cell) {
+            findCell(cell.x + 1, cell.y + 1).addClass(cell.terrain)
+          })
+        }
 
-      rainedOn = setInterval(function () {
-        console.log('check')
-        if ($('.sunny').hasClass('rain')) {
-          console.log('check if sunny')
-          sunny = setInterval(function () {
-            randomSunny = _.random(0, 1)
-            if (randomSunny) {
-              console.log('what')
-              object.off("run", "all", function () {
-                console.log('stop')
-              })
-              // object.off()
-              clearInterval(raining)
-              clearInterval(lightning)
-              $('.sunny').removeClass('rain rain2 lightning')
+        function moveTo(cell) {
+          var coords = findCell(cell.x, cell.y).offset()
+          character.css({
+            top: coords.top,
+            left: coords.left
+          })
+          characterStats.currentPosition = { x: cell.x, y: cell.y }
+          console.log(characterStats.currentPosition)
+          return characterStats.currentPosition
+        }
 
+        function findTerrain(cell) {
+          return cell.attr('class')
+        }
+
+        function findDirection(direction) {
+
+          var x = characterStats.currentPosition.x
+          var y = characterStats.currentPosition.y
+
+          if (direction === 'top') {
+            y--
+          } else if (direction === 'bottom') {
+            y++
+          } else if (direction === 'right') {
+            x++
+          } else if (direction === 'left') {
+            x--
+          }
+          return { x: x, y: y }
+        }
+
+        moveTo(characterStats.currentPosition)
+
+        buildTerrain(cellsTerrain)
+        // for the rain if I decide to use it
+        var raining = {}
+        var lightning = 0
+        var raining = 0
+        var lightningStrike = 0
+        function rain () {    
+          $('.sunny').addClass('rain')
+         
+          raining = setInterval(function () {
+            $('.sunny').toggleClass('rain2')
+          }, 200)
+
+          lightning = setInterval(function () {
+            var randomLightning = _.random(1, 20)
+            $('.sunny').removeClass('lightning')
+            if (randomLightning === 1) {
+              lightningStrike = setTimeout(function () {
+                $('.sunny').addClass('lightning')
+                console.log('lightning')
+                 
+              }, 200)
             }
-          }, 10000)
+             
+          }, 300)
+          console.log('raining')
         }
-      }, 10000)
 
-      checkSunny = setInterval(function () {
-       
-        if ($('.sunny').hasClass('rain') === false) {
-          clearInterval(sunny)
-          console.log('omg')
-        }
-      }, 5000)
-   
-      var object = {};
+        var randomRain = _.random(0)
+        var rainOrNot = 0
+        rainOrNot = setInterval(function () {
+          randomRain = _.random(0, 5)
+          if (randomRain === 1) {
+            if($('.sunny').hasClass('rain')) return false
+            else {
+              object.trigger("run");
+            }  
+          }
+        }, 2000)
 
-      _.extend(object, Backbone.Events);
+        var randomSunny = 0
+        var sunny = 0
+        var rainedOn = 0
+        var checkSunny = 0
 
-      object.on("run", function() {
-        rain();
-        console.log('started raining')
-      });
-      // object.off(null, null, function () {
-      //   console.log('stopped raining')
-      // })
+        rainedOn = setInterval(function () {
+          console.log('check')
+          if ($('.sunny').hasClass('rain')) {
+            console.log('check if sunny')
+            sunny = setInterval(function () {
+              randomSunny = _.random(0, 1)
+              if (randomSunny) {
+                console.log('what')
+                object.off("run", "all", function () {
+                  console.log('stop')
+                })
+                // object.off()
+                clearInterval(raining)
+                clearInterval(lightning)
+                $('.sunny').removeClass('rain rain2 lightning')
+
+              }
+            }, 10000)
+          }
+        }, 10000)
+
+        checkSunny = setInterval(function () {
+         
+          if ($('.sunny').hasClass('rain') === false) {
+            clearInterval(sunny)
+            console.log('omg')
+          }
+        }, 5000)
+     
+        var object = {};
+
+        _.extend(object, Backbone.Events);
+
+        object.on("run", function() {
+          rain();
+          console.log('started raining')
+        });
+        // object.off(null, null, function () {
+        //   console.log('stopped raining')
+        // })
+      })
     })
   }
 });
